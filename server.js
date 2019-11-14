d44e445084d108cd64d541327c339000b7029cf1
RohanBhuyan <rbhuyan@its.jnj.com>
2019-11-14 15:16:11 -0500

    created route templates

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require ('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser');
+
+//var commentRoutes = require("./routes/comments"),
+//    assetRoutes = require("./routes/assets");
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+    
+//app.use(commentRoutes);
+//app.use(assetRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World')
